67abca896459bea6c8a5e31b51a64c8e69eefaf1
Aghata Sophia <aghatasophia@gmail.com>
2020-07-29 09:57:26 -0300

    Creates Logo Component

---
+++ src/components/LogoMain/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+import Logo from '../../assets/img/Logo.png';
+
+const LogoMain = styled.img.attrs({ src: Logo, alt: 'Logo da ComeDevUp'})`
+    height: 40px;
+`;
+
+export default LogoMain;
