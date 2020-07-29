19b189200adc1bfa7d8f8a5a2584d8b99076d83c
Aghata Sophia <aghatasophia@gmail.com>
2020-07-29 09:58:45 -0300

    Creates Link Component and add to the App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import LogoMain from './components/LogoMain';
+import ButtonMain from './components/ButtonMain';
+import Footer from './components/Footer';
+import Link from './components/Link';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header>
+        <LogoMain />
+
+        <ButtonMain>Novo Vídeo</ButtonMain>
+      </Header>
+
+      <Footer>
+        <LogoMain />
+        <span>
+          Site feito na <strong>#ImersãoReact</strong> da <Link href="https://alura.com.br">Alura </Link>
+        </span>
+      </Footer>
+    </>
   );
 }
 
---
+++ src/components/Link/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Link = styled.a`
+    font-weight: bold;
+    text-decoration: none;
+    color: var(--color-primary);
+`;
+
+export default Link;
