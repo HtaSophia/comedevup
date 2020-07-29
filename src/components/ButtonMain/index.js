4b9932522157cb57f497803959aae2f3e81785ca
Aghata Sophia <aghatasophia@gmail.com>
2020-07-29 09:56:44 -0300

    Creates Button Component

---
+++ src/components/ButtonMain/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const ButtonMain = styled.button`
+    font-size: 18rem;
+    color: var(--color-gray-light);
+    background-color: var(--color-black-dark);
+    border-bottom: 1px solid var(--color-gray-light);
+    border-radius: 4px;
+    padding: 14px 35px;
+    cursor: pointer;
+`;
+
+export default ButtonMain;
